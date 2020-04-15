36f2e599293cd9cbbca8dbdde95aeea9bbc39b5e
vkami <vilisicsk@gmail.com>
2020-04-15 11:04:10 +0200

    Add files

--- src/main/java/project/april/web/Wiz1.java
+++ src/main/java/project/april/web/Wiz1.java
@@ -8,23 +8,44 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name="Wiz1", urlPatterns="/wiz1")
+@WebServlet(name = "Wiz1", urlPatterns = "/wiz1")
 public class Wiz1 extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-
-        resp.setContentType("text/html");
-        PrintWriter writer=resp.getWriter();
+        resp.setContentType("text/html; charset=UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
 
         writer.println("<!DOCTYPE html><html>");
         writer.println("<head>");
+        writer.println("<link rel=\"stylesheet\" href=\"style.css\">");
         writer.println("<meta charset=\"UTF-8\" />");
-        writer.println("<title>Hello World!</title>");
+
+        writer.println("<title>Wizard 1</title>");
         writer.println("</head>");
         writer.println("<body>");
-        writer.println("<h1>Hello World</h1>");
+        writer.println("<div id=\"mainContainer\">");
+
+        writer.println("<h2>Elso lepes</h2>");
+        writer.println("<h3>Valassz egy termeket!</h3>");
+
+        writer.println("<p>Termekeink:</p>");
+
+        writer.println("<form action=\"wiz2\" method=\"POST\" id=\"form1\">");
+        writer.println("<select name=\"good\">");
+        writer.println("<option value=\"kavefozo\">kavefozo</option>");
+        writer.println("<option value=\"hosugarzo\">hosugarzo</option>");
+        writer.println("<option value=\"mikro\">mikrohullamu suto</option>");
+
+        writer.println("</select>");
+        writer.println("</form>");
+
+        writer.println("<a href=\"#\" onclick=\"document.getElementById('form1').submit();\">Tovabb</a>");
+
+        writer.println("</div>");
+
         writer.println("</body>");
         writer.println("</html>");
 
---
+++ src/main/java/project/april/web/Wiz3.java
@@ -0,0 +1,64 @@
+package project.april.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "Wiz3", urlPatterns = "/wiz3")
+public class Wiz3 extends HttpServlet {
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String name=req.getParameter("name");
+        String address=req.getParameter("address");
+
+        HttpSession session=req.getSession();
+        session.setAttribute("name",name);
+        session.setAttribute("address",address);
+
+        doGet(req, resp);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        HttpSession session=req.getSession();
+        Object good=session.getAttribute("good");
+        Object name=session.getAttribute("name");
+        Object address=session.getAttribute("address");
+
+        resp.setContentType("text/html");
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<!DOCTYPE html><html>");
+        writer.println("<head>");
+        writer.println("<link rel=\"stylesheet\" href=\"style.css\">");
+        writer.println("<title>Wizard 3</title>");
+        writer.println("</head>");
+        writer.println("<body>");
+        writer.println("<div id=\"mainContainer\">");
+
+        writer.println("<h2>Osszesites</h2>");
+
+        writer.println("<p>A kovetkezo termekeket rendelted meg: " + (String)good + "</p>");
+        writer.println("<p>Vevo neve: " + (String)name + "</p>");
+        writer.println("<p>Szallitasi cim: " + (String)address + "</p>");
+
+
+        writer.println("<a href=\"end\">Rendben fizetek</a>");
+
+        writer.println("</div>");
+
+        writer.println("</body>");
+        writer.println("</html>");
+
+
+    }
+}
